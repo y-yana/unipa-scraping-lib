94154bfc99c85595d3329f270ac25f2d839290e7
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-05-24 21:21:21 +0900

    cli test

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="test_code",
-    version='1.0',
+    version='1.1',
     description='test',
     author='y-yana',
     url='https://github.com/y-yana/unipa-scraping-lib',
+    packages=find_packages(),
+    entry_points="""
+      [console_scripts]
+      test_code = test_code.cli_test:execute
+    """,
 )
