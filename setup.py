e1ab2bae05fc91dcad71e587f5e963435cfa75ad
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-05-24 21:17:12 +0900

    library test

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="test_code",
+    version='1.0',
+    description='test',
+    author='y-yana',
+    url='https://github.com/y-yana/unipa-scraping-lib',
+)
---
+++ test_code/test_code.py
@@ -0,0 +1,2 @@
+def call():
+    print("Hello World")
