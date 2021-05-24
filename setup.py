b0de097f4d5a1873bff57e839fa173511464d7ee
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-05-24 22:09:11 +0900

    add command

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="test_code",
-    version='1.2',
+    version='1.3',
     description='test',
     author='y-yana',
     url='https://github.com/y-yana/unipa-scraping-lib',
     packages=find_packages(),
     entry_points="""
       [console_scripts]
-      test_code = test_code.cli_test:execute
+      test_code = test_code.test_code:call
+      cli_test = test_code.cli_test:execute
     """,
     install_requires=open('requirements.txt').read().splitlines(),
 )
